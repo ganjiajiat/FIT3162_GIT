75cb33db8501e7e7957553fafe54519bca00a99b
Jason-YueNamYoong <69240589+Jason-YueNamYoong@users.noreply.github.com>
2020-10-21 14:41:42 +0800

    Update flask trial.py

--- Web/v2/flask trial.py
+++ Web/v2/flask trial.py
@@ -11,24 +11,7 @@ def root():
 def index():
     return render_template('index.html')
 
-# @app.route("/predictor")
-# def predictor():
-#     return render_template('predictor.html')
 
-# @app.route("/prediction", methods=["GET","POST"])
-# def prediction():
-#     req = request.form
-#     print(req)
-#     age = int(request.form.get("age"))
-#     # bmi = request.form.get("bmi")
-#     # symptom_1 = request.form.get("symptom_1")
-#     result=str(calculation(age))
-#
-#     return render_template('index.html',age=result)
-
-def calculation(age):
-    ans=age+10
-    return ans
 
 if __name__ == '__main__':
   app.run()
